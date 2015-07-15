446bc0a8eebef488e644540f7ffea37d9bdd76b8
Michal Ostruszka <michal.ostruszka@gmail.com>
2015-07-15 09:17:12 +0200

    Fixed formatting

--- test/keyhole.spec.js
+++ test/keyhole.spec.js
@@ -4,51 +4,51 @@ import keyhole from '../src/keyhole';
 
 describe('keyhole', () => {
 
-	const source = { 
-		name: 'john', 
-		age: 29, 
-		emails: {
-			priv: 'priv@email.com',
-			work: 'work@email.com',
-		},
-		foo: {
-			bar: {
-				baz: ['foo', 'bar', 'baz']
-			}
-		} 
-	};
+  const source = { 
+    name: 'john', 
+    age: 29, 
+    emails: {
+      priv: 'priv@email.com',
+      work: 'work@email.com',
+    },
+    foo: {
+      bar: {
+        baz: ['foo', 'bar', 'baz']
+      }
+    } 
+  };
 
-	describe('nonexistent or invalid properties', () => {
+  describe('nonexistent or invalid properties', () => {
 
-		it('should be ignored', () => {
-			const res = keyhole(source, 'name', 'age', 'non.existent', 'emails.nonexistent');
-			expect(res).toEqual({ name: 'john', 'age': 29 });
-		});
+    it('should be ignored', () => {
+      const res = keyhole(source, 'name', 'age', 'non.existent', 'emails.nonexistent');
+      expect(res).toEqual({ name: 'john', 'age': 29 });
+    });
 
-		it('should result with empty object when no properties given', () => {
-			const res = keyhole(source);
-			expect(res).toEqual({});
-		});
+    it('should result with empty object when no properties given', () => {
+      const res = keyhole(source);
+      expect(res).toEqual({});
+    });
 
-	});
+  });
 
-	describe('nested properties', () => {
-		
-		it('should be properly resolved', () => {
-			const res = keyhole(source, 'emails.priv');
-			expect(res).toEqual({ emails: { priv: 'priv@email.com' } });
-		});
+  describe('nested properties', () => {
+    
+    it('should be properly resolved', () => {
+      const res = keyhole(source, 'emails.priv');
+      expect(res).toEqual({ emails: { priv: 'priv@email.com' } });
+    });
 
-		it('should be copied as a whole', () => {
-			const res = keyhole(source, 'emails');
-			expect(res).toEqual({ emails: { priv: 'priv@email.com', work: 'work@email.com' } });
-		});
+    it('should be copied as a whole', () => {
+      const res = keyhole(source, 'emails');
+      expect(res).toEqual({ emails: { priv: 'priv@email.com', work: 'work@email.com' } });
+    });
 
-		it('should be resolved at any depth', () => {
-			const res = keyhole(source, 'foo.bar.baz');
-			expect(res).toEqual({ foo: { bar: { baz: ['foo', 'bar', 'baz'] } } });
-		});
+    it('should be resolved at any depth', () => {
+      const res = keyhole(source, 'foo.bar.baz');
+      expect(res).toEqual({ foo: { bar: { baz: ['foo', 'bar', 'baz'] } } });
+    });
 
-	});
+  });
 
 });
